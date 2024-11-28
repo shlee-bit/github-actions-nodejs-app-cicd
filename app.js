a80f30bdfcb10296eac8d035dcc8fad8feb2e73e
shlee <2391051@hansung.ac.kr>
2024-11-28 22:16:38 +0900

    finals

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('hello world\n');
+  res.end('nodeJs application practice\n');
 });
 
 server.listen(port, hostname, () => {
--- app.test.js
+++ app.test.js
@@ -9,7 +9,7 @@ const server = require('./app');
 chai.use(chaiHttp);
 
 describe('HTTP Server', () => {
-  it('should return "Hello, World!" when GET /', (done) => {
+  it('should return "nodeJs application practice" when GET /', (done) => {
     chai.request(server)
       .get('/')
       .end((err, res) => {
